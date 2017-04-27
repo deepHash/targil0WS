4c34394c896b3886e6564870e614417483402fe6
deepHash <itsesisx@gmail.com>
2017-04-27 15:22:04 +0300

    added bonus

--- athlete/index.js
+++ athlete/index.js
@@ -3,10 +3,11 @@ var EventEmitter = require('events');
 
 class Athlete extends EventEmitter{
     
-    //update to hold numOfMedals in constructor with a default value of 0
+    //class constructor, recieves the athletes name and field in sports
     constructor(name, field) {
         super();
         var output;
+        this.array = [];
         this.name = name;
         this.field = field;
         this.numOfMedals = 0;
@@ -22,8 +23,9 @@ class Athlete extends EventEmitter{
     decreaseMedals() {
         //checks if the number of medals is zero before decreasing 
         if (this.numOfMedals == 0){
-            this.output += `Error: ${this.name} has zero medals\n`;
+            this.output = `Error: ${this.name} has zero medals\n`;
             console.log(this.output);
+            this.writeToLog(this.output);
         }
         else {
             this.numOfMedals--;
@@ -40,13 +42,20 @@ class Athlete extends EventEmitter{
     }
 
     //emit method
-    medalsChanged(){
-        this.output += `${this.name}s medals count has changed to: ${this.numOfMedals} in ${this.field}\n`;
+    medalsChanged() {
+        this.output = `${this.name}s medals count has changed to: ${this.numOfMedals} in ${this.field}\n`;
         console.log(this.output);
+        this.writeToLog(this.output);
     }
 
+    //takes data, that was written to console log and pushes it into array log
+    writeToLog(data) {
+        this.array.push(data);
+    }
+
+    //outputs the data we printed and pushed to the array
     getOutput() {
-        return this.output;
+        return this.array;
     }
 
 };
--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 var express = require('express');
-var athlete = require('./athlete');
-var EventEmitter = require('events');
-
-var app = express()
+    athlete = require('./athlete');
+    app = express()
+    athletes = [];
+    data = [];
 
 //first athlete
-var a1 = athlete("Gordie Howe", "Hockey");
+var firstAthlete = athlete("Gordie Howe", "Hockey");
+athletes.push(firstAthlete);
 
-a1.increaseMedals();
-a1.decreaseMedals();
-a1.decreaseMedals();
-a1.increaseMedals();
-a1.increaseMedals();
+firstAthlete.increaseMedals();
+firstAthlete.decreaseMedals();
+firstAthlete.decreaseMedals();
+firstAthlete.increaseMedals();
+firstAthlete.increaseMedals();
 
-var data = a1.getAllData();
-//var data = a1.getOutput();
+//second athlete
+var secondAthlete = athlete("Michael Phelps", "Swimming");
+athletes.push(secondAthlete);
 
+secondAthlete.increaseMedals();
+secondAthlete.increaseMedals();
+secondAthlete.increaseMedals();
+secondAthlete.decreaseMedals();
+secondAthlete.increaseMedals();
+
+//adding all the data that we have so we can send it to the client
+for (var i=0; i<athletes.length; i++) {
+    data.push({Athlete: athletes[i].getAllData(),
+                 Messages: athletes[i].getOutput()}); 
+}
 
 app.get('/', function (req, res) {
     res.send(data);
 })
 
+//var port = process.env.PORT || 8080
 var server = app.listen(8080, function() {
     //var host = server.address().address;
     var port = server.address().port;
